3df1ab692d5c0637469ad155b8bd92631e8744e8
DALI HILLARY <dalirichardh@gmail.com>
2021-07-30 10:46:26 +0300

    navigation

---
+++ app/src/main/java/group/asteriskint/adm/ui/fragment/AccountFragment.kt
@@ -0,0 +1,32 @@
+package group.asteriskint.adm.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import group.asteriskint.adm.R
+
+class AccountFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AccountFragment()
+    }
+
+    private lateinit var viewModel: AccountViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.account_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AccountViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
