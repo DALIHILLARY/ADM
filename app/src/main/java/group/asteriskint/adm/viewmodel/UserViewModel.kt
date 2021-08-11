76cf30e3fb03eb075ecb1ac094aa75644d35ab8a
DALI HILLARY <dalirichardh@gmail.com>
2021-08-11 11:22:41 +0300

    cart x2

--- app/src/main/java/group/asteriskint/adm/viewmodel/UserViewModel.kt
+++ app/src/main/java/group/asteriskint/adm/viewmodel/UserViewModel.kt
@@ -5,13 +5,10 @@ import androidx.lifecycle.ViewModel
 import group.asteriskint.adm.model.User
 
 class UserViewModel : ViewModel() {
-    val user : MutableLiveData<User>  by lazy {
-        MutableLiveData<User>().also {
-            null
-        }
-     }
+    val user = MutableLiveData<User>(null)
 
     fun login(email: String, password: String) : Boolean {
-        TODO("do network call and save to dbn return result")
+//        TODO("do network call and save to dbn return result")
+        return true
     }
 }
